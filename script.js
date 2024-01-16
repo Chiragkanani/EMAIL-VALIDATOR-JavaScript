504de3399c83d5f90f06bbd6b70052201ce19366
Chiragkanani <90540657+Chiragkanani@users.noreply.github.com>
2024-01-16 14:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,27 @@ let p = document.querySelector("#resultcon p");
 let str = ""
 btn.addEventListener("click",async(e)=>{
     e.preventDefault();
-        p.remove()
-        loading.src="img/loading.svg";
+       
         let value = email.value;
-        console.log(value)
-    let fetched = await fetch(`https://api.emailvalidation.io/v1/info?apikey=${api}&email=${value}`)
-    let data = await fetched.json();
+        if(value!==""){
+            console.log(value)
+            p.remove()
+            loading.src = "img/loading.svg";
+            let fetched = await fetch(`https://api.emailvalidation.io/v1/info?apikey=${api}&email=${value}`)
+            let data = await fetched.json();
 
-  for (const key of Object.keys(data)) {
-    if(data[key]!==""&& data[key]!==" "){
-        str = str + `<div>${key}:${data[key]}`
-    }
-    
-}
-    loading.src="";
-    resultcon.innerHTML = str;
+            for (const key of Object.keys(data)) {
+                if (data[key] !== "" && data[key] !== " ") {
+                    str = str + `<div>${key}:${data[key]}`
+                }
 
+            }
+            loading.src = "";
+            resultcon.innerHTML = str;
+            str = "";
+
+        }
+        
 
 })
 
